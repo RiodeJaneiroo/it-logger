e2dc126e70049ae7675be705371d2cbe7fd7c95b
Vadim Zmiievskyi <prainua@gmail.com>
2021-01-23 11:55:49 +0200

    implement techs list modal

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { useEffect, Fragment } from 'react';
 import SearchBar from './components/layout/SearchBar';
 import Logs from './components/logs/Logs';
+import AddBtn from './components/layout/AddBtn';
+import AddLogModal from './components/layout/AddLogModal';
+import EditLogModal from './components/logs/EditLogModal';
+import AddTechModal from './components/tech/AddTechModal';
+import TechListModal from './components/tech/TechListModal';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
@@ -13,6 +18,11 @@ const App = () => {
 		<Fragment>
 			<SearchBar />
 			<div className='container'>
+				<AddBtn />
+				<AddLogModal />
+				<EditLogModal />
+				<AddTechModal />
+				<TechListModal />
 				<Logs />
 			</div>
 		</Fragment>
